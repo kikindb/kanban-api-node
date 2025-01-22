a9a14d8b8a5dc48dab77249e96a2b3f9be395da5
= <=>
2025-01-22 14:04:44 -0600

    fix: add prod domain to cors

--- startup/routes.js
+++ startup/routes.js
@@ -24,6 +24,8 @@ module.exports = function (app) {
         'http://localhost:5000',
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'https://kikindb.github.io/react-kanban',
+        'https://kikindb.github.io',
         '*',
       ],
       credentials: true,
